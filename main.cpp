36a5f7377107b8a547b1c5bc97e7cfa73a389b2f
andrewstephengames <andrewstephengames999@gmail.com>
2024-02-21 21:27:03 +0200

    Add raylib window

--- main.cpp
+++ main.cpp
@@ -1,5 +1,28 @@
-#include <iostream>
+#include "snake.h"
 
 int main () {
-     std::cout << "Hello World\n";
+     int factor = 100;
+     Vector2 w = {
+          .x = factor*16,
+          .y = factor*9,
+     };
+     srand(clock());
+     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+     SetTraceLogLevel(LOG_ERROR);
+     InitWindow (w.x, w.y, "Snake");
+     Game *game = new Game (Menu, w, {0}, 0);
+     SetTargetFPS(60);
+     while (!WindowShouldClose()) {
+          w.x = GetScreenWidth();
+          w.y = GetScreenHeight();
+          game->set_canvas (w);
+          BeginDrawing();
+               game->state_machine();
+          EndDrawing();
+          if (IsKeyPressed (KEY_Q)) {
+               break;
+          }
+     }
+     CloseWindow();
+     return 0;
 }
